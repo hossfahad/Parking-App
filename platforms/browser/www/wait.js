dae18336f0d7e1892acfa1456483a92345b65ec3
Fahad H <hoss.fahad@gmail.com>
2018-04-29 10:23:02 -0400

    more changes

---
+++ platforms/browser/www/wait.js
@@ -0,0 +1,6 @@
+for (i=0;i>10;i++) {
+    if (guess == 6) {
+        console.log("That's right! You're a good guesser!")
+    } else {
+        console.log("That is incorrect. Guess again.");
+    };
